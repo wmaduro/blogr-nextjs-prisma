d237694e1ebfbff5c149463db16751932fe024e9
Mahmoud Abdel Wahab <mahmoud3799@gmail.com>
2020-11-26 18:12:39 +0200

    update

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -30,6 +30,13 @@ async function publishPost(id: number): Promise<void> {
   await Router.push("/");
 }
 
+async function deletePost(id: number): Promise<void> {
+  await fetch(`http://localhost:3000/api/post/${id}`, {
+    method: "DELETE",
+  });
+  Router.push("/");
+}
+
 const Post: React.FC<PostProps> = (props) => {
   const [session, loading] = useSession();
   if (loading) {
@@ -50,6 +57,9 @@ const Post: React.FC<PostProps> = (props) => {
         <ReactMarkdown source={props.content} />
         {!props.published && userHasValidSession && postBelongsToUser && (
           <button onClick={() => publishPost(props.id)}>Publish</button>
+        )}
+        {userHasValidSession && postBelongsToUser && (
+          <button onClick={() => deletePost(props.id)}>Delete</button>
         )}
       </div>
       <style jsx>{`
